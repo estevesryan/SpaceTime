9392697f5b975dd416c8e8d20f1709e34893e702
Ryan Esteves <ryanplay71@gmail.com>
2023-05-19 16:08:22 -0300

    style(styles modifications): :art:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@ export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
-      <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16 ">
+      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white-100/10 px-28 py-16">
         {/* blur */}
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
 
         {/* stripes */}
-        <div className="absolute bottom-0 right-1 top-0 w-2 bg-stripes" />
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
       </div>
 
       {/* Right */}
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
     },
 
     colors: {
+      white: { 100: '#ffff' },
       gray: {
         50: '#eaeaea',
         100: '#bebebf',
