3474808ad2b67ef08548a7d7962a27411c8f70a9
Ryan Esteves <ryanplay71@gmail.com>
2023-05-21 11:50:31 -0300

    feat(implementações): :art: buscar os dados do usuario e mostra em tela

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,25 @@
+import { cookies } from 'next/headers'
+
 import Copyright from '@/components/Copyright/copyright'
 import HeroSection from '@/components/HeroSection/HeroSection'
 import SignIn from '@/components/SignIn/SignIn'
 import Stripe from '@/components/Stripe/Stripe'
-import Blur from '@/components/blur/Blur'
+import Blur from '@/components/Blur/Blur'
+import Profile from '@/components/Profile/Profile'
 
 export default function Home() {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
-      <div className="bg-cover relative flex flex-col items-start justify-between overflow-hidden border-r border-white-100/10 bg-[url(../assets/bg-stars.svg)] px-28 py-16">
+      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white-100/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
         {/* blur */}
         <Blur />
         {/* stripes */}
         <Stripe />
         {/* SINGIN */}
-        <SignIn />
+        {isAuthenticated ? <Profile /> : <SignIn />}
         {/* hero section */}
         <HeroSection />
 
@@ -23,7 +28,7 @@ export default function Home() {
       </div>
 
       {/* Right */}
-      <div className="bg-cover flex flex-col bg-[url(../assets/bg-stars.svg)] p-16">
+      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
         <div className="flex flex-1 items-center justify-center">
           <p className="w-[360px] text-center leading-relaxed">
             Você ainda não registrou nenhuma lembrança, comece a{' '}
---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,26 @@
+import { getUser } from '@/lib/auth'
+import Image from 'next/image'
+
+export default function Profile() {
+  const { name, avatar_url } = getUser()
+  console.log(avatar_url)
+  return (
+    <div className="flex items-center gap-3 text-left">
+      {/* profile photo */}
+      <Image
+        className="h-10 w-10 rounded-full"
+        width={40}
+        height={40}
+        src={avatar_url}
+        alt="profile picture"
+      />
+
+      <p className="max-w-[140px] text-sm leading-snug">
+        {name}
+        <a href="" className="block text-red-400 hover:text-red-300">
+          Quero sair
+        </a>
+      </p>
+    </div>
+  )
+}
